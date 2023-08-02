c7a650bb8e04b4651b84bd2542ca82905d1ab4ac
Joy Fan <chandler0204@gmail.com>
2023-08-02 16:57:13 +0800

    feat:show map

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Mapbox from 'map'
+// import { Source, Layer } from 'react-map-gl'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Mapbox
+        initialViewState={{
+          latitude: 23.144864319264016,
+          longitude: 120.2458966147924,
+          zoom: 11
+        }}
+      ></Mapbox>
     </>
   )
 }
--- src/Map/index.tsx
+++ src/Map/index.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
-import { Map } from 'react-map-gl'
+import { useRef, ReactNode } from 'react'
+import { Map, ViewState } from 'react-map-gl'
+// import { useSearchParams } from 'react-router-dom'
 
-const Mapbox = () => {
-  return <Map />
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+interface MapboxProps {
+  initialViewState: Partial<ViewState>
+  children?: ReactNode
+}
+const Mapbox = ({ initialViewState, children }: MapboxProps) => {
+  // const [searchParams] = useSearchParams()
+  const mapRef = useRef(null)
+  // const mapRefCallback = useCallback((map) => {
+
+  // })
+  return (
+    <Map
+      initialViewState={{
+        latitude: initialViewState.latitude,
+        longitude: initialViewState.longitude,
+        zoom: initialViewState.zoom
+      }}
+      ref={mapRef}
+      mapStyle={`mapbox://styles/mapbox/streets-v12`}
+      mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
+      // interactiveLayerIds={layerIds || []}
+      // onClick={onMapClick}
+      // onZoomEnd={onZoom}
+      // onLoad={onLoad}
+      minZoom={5}
+      maxZoom={15}
+      maxBounds={[
+        [115, 20],
+        [125, 28]
+      ]}
+    >
+      {children}
+    </Map>
+  )
 }
 
 export default Mapbox

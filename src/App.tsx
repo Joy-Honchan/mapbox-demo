811bb5677b19791f10f07e60ddc01fd81bd161c5
Joy Fan <chandler0204@gmail.com>
2023-08-11 17:05:55 +0800

    setting: install tailwind and add data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css'
-import { KH_MRT_DATA } from 'data/KH-MART'
-import Mapbox from 'map'
-import ReactSvgUrl from 'assets/react.svg'
+// import { KH_MRT_DATA } from 'data/KH-MART'
+// import Mapbox from 'map'
+// import ReactSvgUrl from 'assets/react.svg'
 
 function App() {
-  return <Mapbox dataset={KH_MRT_DATA} iconData={{ react: ReactSvgUrl }} />
+  // return <Mapbox dataset={KH_MRT_DATA} iconData={{ react: ReactSvgUrl }} />
+  return (
+    <section className=" h-screen flex flex-col">
+      <header className="bg-orange-500 h-16">Data in Tainan</header>
+      <section className="flex flex-grow">
+        <div className="bg-orange-300 w-64">SideBar</div>
+        <section className=" bg-blue-500 flex-grow">
+          <main>Map</main>
+        </section>
+      </section>
+    </section>
+  )
 }
 
 export default App
